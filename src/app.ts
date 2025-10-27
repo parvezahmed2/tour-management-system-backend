a0f462026949f09f38e7f4dfe546f8d3d59361fe
parvezahmed2 <parvezahmed9432@gmail.com>
2025-10-27 09:29:16 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+ 
+import express, { Request, Response } from "express"
+ 
+
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message : "Welcome to tour management System Backend"
+    })
+})
+
+
+export default app
+
+
+
+/**
+ * unhandled rejection error 
+ * uncaught rejection error 
+ * 
+ */
---
+++ src/server.ts
@@ -0,0 +1,77 @@
+import {Server} from "http"
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+ 
+
+let server : Server;
+ 
+
+
+const startServer = async() =>{
+   try{
+         await mongoose.connect(envVars.DB_URL)
+
+    console.log("Conected to DB");
+    server = app.listen(envVars.PORT, () => {
+        console.log(`Server is listening to port 5000 ${envVars.PORT}`)
+    })
+   }
+   catch(error){
+    console.log(error)
+   }
+}
+
+startServer()
+
+process.on("unhandledRejection", (err)=> {
+    console.log("Unhandled Rejection detected... Server Shutting down..", err);
+    if(server){
+        server.close(()=>{
+            process.exit(1) // node js server off 
+        });
+    }
+    process.exit(1) // node js server off 
+})
+
+process.on("uncaughtException", (err)=> {
+    console.log("Uncaught Exception detected... Server Shutting down..", err);
+    if(server){
+        server.close(()=>{
+            process.exit(1) // node js server off 
+        });
+    }
+    process.exit(1) // node js server off 
+})
+
+
+process.on("SIGTERM", (err)=> {
+    console.log("SIGTERM signal recieved.   Server Shutting down..", err);
+    if(server){
+        server.close(()=>{
+            process.exit(1) // node js server off 
+        });
+    }
+    process.exit(1) // node js server off 
+})
+
+
+process.on("SIGINT", (err)=> {
+    console.log("SIGINT signal recieved.   Server Shutting down..", err);
+    if(server){
+        server.close(()=>{
+            process.exit(1) // node js server off 
+        });
+    }
+    process.exit(1) // node js server off 
+})
+
+
+
+// unhandler rejection error
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+// uncaught excepton 
+// throw new Error("I forgot to handle this local error")
+
+
